081d63fc0c240c10164dfc70674cdb83aaf74ed2
Beatrice Gobbo <gobbo.beatrice92@gmail.com>
2021-01-25 16:31:20 +0100

    edit svg

--- code/components/phase-01.js
+++ code/components/phase-01.js
@@ -1,24 +1,28 @@
 import React from 'react'
 
 const PhaseOne = props => (
-  <svg
-    id="prefix__Layer_1"
-    x={0}
-    y={0}
-    viewBox="0 0 400 300"
-    xmlSpace="preserve"
-    style={{strokeWidth: 1}}
-    {...props}
-  >
-    <style>
-      {
-        '.prefix__st0{fill:none;stroke:var(--accent);stroke-miterlimit:10}'
-      }
-    </style>
-    <circle className="prefix__st0" cx={199.8} cy={149.8} r={147.9} />
-    <circle className="prefix__st0" cx={148.9} cy={149.8} r={147.9} />
-    <circle className="prefix__st0" cx={250.2} cy={150.7} r={147.9} />
-    <path className="prefix__st0" d="M347.7 152.1" />
+<svg id="prefix__Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 400 300" x={0} y={0}>
+
+<g>
+    <rect x="128.8905" y="12.9874" width="22.4029" height="225.8266" fill="none" stroke="#000" stroke-miterlimit="10"/>
+    <rect x="65.8088" y="114.6184" width="224.6948" height="22.2906" transform="translate(35.2081 290.4512) rotate(-84.2611)" fill="none" stroke="#000" stroke-miterlimit="10"/>
+    <rect x="103.3072" y="114.4252" width="225.8266" height="22.4029" transform="translate(50.0808 312.525) rotate(-78.5222)" fill="none" stroke="#000" stroke-miterlimit="10"/>
+  </g>
+  <g>
+    <rect x="12.3669" y="12.9874" width="22.4029" height="225.8266" fill="none" stroke="#000" stroke-miterlimit="10"/>
+    <rect x="50.4435" y="12.9747" width="22.3783" height="225.578" transform="translate(-5.8322 3.0299) rotate(-2.689)" fill="none" stroke="#000" stroke-miterlimit="10"/>
+    <rect x="88.4954" y="12.7134" width="22.4029" height="225.8266" transform="translate(-11.3354 9.897) rotate(-5.3779)" fill="none" stroke="#000" stroke-miterlimit="10"/>
+  </g>
+  <g>
+    <rect x="129.3163" y="12.6499" width="22.4029" height="225.8266" fill="none" stroke="#000" stroke-miterlimit="10"/>
+    <rect x="66.2347" y="114.2808" width="224.6948" height="22.2906" transform="translate(35.9272 290.5711) rotate(-84.2611)" fill="none" stroke="#000" stroke-miterlimit="10"/>
+    <rect x="103.733" y="114.0877" width="225.8266" height="22.4029" transform="translate(50.7527 312.6719) rotate(-78.5222)" fill="none" stroke="#000" stroke-miterlimit="10"/>
+  </g>
+  <g>
+    <rect x="12.7927" y="12.6499" width="22.4029" height="225.8266" fill="none" stroke="#000" stroke-miterlimit="10"/>
+    <rect x="50.8693" y="12.6372" width="22.3783" height="225.578" transform="translate(-5.8159 3.0495) rotate(-2.689)" fill="none" stroke="#000" stroke-miterlimit="10"/>
+    <rect x="88.9213" y="12.3758" width="22.4029" height="225.8266" transform="translate(-11.3019 9.9355) rotate(-5.3779)" fill="none" stroke="#000" stroke-miterlimit="10"/>
+  </g>
   </svg>
 )
 
