e3c62d2b8b2d5657c01651203c4bd34d7701d622
Federico <federico.izzop@gmail.com>
2021-02-09 16:21:41 +0100

    group04_website

---
+++ docs/course-results/es03/group04/assets/js/eyebtn.js
@@ -0,0 +1,47 @@
+let url = new URL(document.location);
+
+let prev = url.searchParams.get('prev');
+
+if (prev === 'Mex') {
+  document.getElementById("eyebtn").innerHTML = `
+          <a href="mexico.html">
+            <span class="backButton"><i class="fas fa-chevron-left"></i> MEXICO</span>
+          </a>
+                `
+} else if (prev === 'Thai') {
+  document.getElementById("eyebtn").innerHTML = `
+          <a href="thailand.html">
+            <span class="backButton"><i class="fas fa-chevron-left"></i> THAILAND</span>
+          </a>
+                `
+} else if (prev === 'Col') {
+  document.getElementById("eyebtn").innerHTML = `
+          <a href="colombia.html">
+            <span class="backButton"><i class="fas fa-chevron-left"></i> COLOMBIA</span>
+          </a>
+                `
+} else if (prev === 'Turk') {
+  document.getElementById("eyebtn").innerHTML = `
+          <a href="turkey.html">
+            <span class="backButton"><i class="fas fa-chevron-left"></i> TURKEY</span>
+          </a>
+                `
+} else if (prev === 'SKor') {
+  document.getElementById("eyebtn").innerHTML = `
+          <a href="southKorea.html">
+            <span class="backButton"><i class="fas fa-chevron-left"></i> SOUTH KOREA</span>
+          </a>
+                `
+} else if (prev === 'RDom') {
+  document.getElementById("eyebtn").innerHTML = `
+          <a href="dominicanRep.html">
+            <span class="backButton"><i class="fas fa-chevron-left"></i> DOMINICAN REPUBLIC</span>
+          </a>
+                `
+} else {
+  document.getElementById("eyebtn").innerHTML = `
+          <a href="eye.html">
+            <span class="backButton"><i class="fas fa-chevron-left"></i> EYE</span>
+          </a>
+                `
+}
